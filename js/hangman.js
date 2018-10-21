ffee4dc1035361cc1017f58a08a92ea76cc94fff
JamalHam <hamilton.jamal.anthony@gmail.com>
2018-10-21 17:36:35 -0400

    changes

--- js/hangman.js
+++ js/hangman.js
@@ -9,3 +9,24 @@ for(var i = 0; i < answerArray.length; i++){
 }
 
 var leftoverLetters=hiddenSelection.length;
+
+while (leftoverLetters > 0){
+    
+    alert(answerArray.join(" "));
+    
+    var guess = prompt("Guess a letter or quit");
+    
+    if(guess === null){
+        break} else if(guess.length==!1){
+            alert("please enter a single letter")
+        }else{
+            for( var j = 0; j < hiddenSelection.length; j++){
+                if (hiddenSelection[j] === guess){
+                    answerArray[j] = guess;
+                    hiddenSelection--;
+                }
+            
+            }
+        }
+    
+}
