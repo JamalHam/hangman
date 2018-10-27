4803c1f8025092e7cc823d0775e682efe26416df
JamalHam <hamilton.jamal.anthony@gmail.com>
2018-10-27 15:47:06 -0400

    updates

--- js/hangman.js
+++ js/hangman.js
@@ -1,13 +1,4 @@
-var anime=["naruto", "bleach", "onepiece", "dragonball", "inuyasha", "hunterxhunter", "pokemon", "digimon","monster", "onepunchman", "fairytail", "akira"]
-
-
-
-var answerArray =[];
-for(var i = 0; i < answerArray.length; i++){
-    answerArray[i] = "_";
-}
-
-var leftoverLetters=hiddenSelection.length;
+var anime=["naruto", "bleach", "onepiece", "dragonball", "inuyasha", "hunterxhunter", "pokemon", "digimon", "monster", "onepunchman", "fairytail", "akira"]
 
 
 var maxAttempts = 10; // maximum number of attempts
