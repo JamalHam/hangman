74251db52a89eb1da249a900764231b6e9a46830
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-02-05 17:44:42 -0500

    updates

--- js/hangman.js
+++ js/hangman.js
@@ -1,42 +1,51 @@
-var anime=["naruto", "bleach", "onepiece", "dragonball", "inuyasha", "hunterxhunter", "pokemon", "digimon", "monster", "onepunchman", "fairytail", "akira"];
+var selectableWords=["naruto", "bleach", "onepiece", "dragonball", "inuyasha", "hunterxhunter", "pokemon", "digimon", "monster", "onepunchman", "fairytail", "akira"];
 
 
-const maxAttempts = 10; // maximum number of attempts
+const maxTries = 10; // maximum number of attempts
 var guessedLetters = []; //guessed letter empty array
-var currentWord; //index of the current word in the array
+var currentWordIndex; //index of the current word in the array
 var guessingWord = []; //word that is slowly built by the user as they try to match the array
 var remainingGuesses = 0; //number of guesses left
 var gameStarted = false; //start status
-var gameFinished = false; //end status
+var hasFinished = false; //end status
 var wins = 0; //Number of wins gained
 
+///////////////////////////////////////////////////////////////Acurate
 
-
-// Reset our game-level variables
 function resetGame() {
-    remainingGuesses = maxAttempts;
+    remainingGuesses = maxTries;
     gameStarted = false;
 
     // Use Math.floor to round the random number down to the nearest whole.
-    currentWordIndex = anime[Math.floor(Math.random() * anime.length)];
+    currentWordIndex = Math.floor(Math.random() * (selectableWords.length));
 
     // Clear out arrays
     guessedLetters = [];
     guessingWord = [];
-
+	
+	document.getElementById('hangmanImage').src = "";
+	
     // Build the guessing word and clear it out
     for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
         guessingWord.push("_");
     }
-
-
+	
+	//hide game over and win images
+	document.getElementById("pressKeytryAgain").style.cssText = "display: none";
+	document.getElementById("gameover-image").style.cssText = "display: none";
+	document.getElementById("youwin-image").style.cssText = "display: none";
+	
     // Show display
     updateDisplay();
 };
 
-function updateDisplay{
+///reset function complete
+
+//display function start
+
+function updateDisplay() {
     document.getElementById("totalWins").innerText = wins;
-    document.getElementById("currentWord").innerText =" ";
+    document.getElementById("currentWord").innerText = "";
     
     for(var i = 0; i < guessingWord.length; i++){
         document.getElementById("currentWord").innerText += guessingWord[i];
@@ -45,59 +54,15 @@ function updateDisplay{
     document.getElementById("remainingGuesses").innerText = remainingGuesses;
     document.getElementById("guessedLetters").innerText = guessedLetters;
     
-    if(remainingGuesses <= 0 ){
+    if(remainingGuesses <= 0){
+		document.getElementById("gameover-image").style.cssText = "display: block";
         document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
-        gameFinished = true;
+        hasFinished = true;
     }
 }
+//update display finished
 
-document.onkeydown = function(event){
-    if(gameFinished){
-        resetGame();
-        gameFinished = false;
-    }else{
-        if(event.keycode >= 65 && event.keycode =< 90){
-            makeGuess(event.key.toLowerCase());
-        }
-    }
-}
-
-function makeGuess(letter){
-    if(remainingGuesses > 0){
-        if(!gameStarted){
-            gameStarted = true;
-        }//checking letters to see if they have been pushed
-        if(guessedLetters.indexOf(letter) === -1){
-        guessedLetters.push(letter);
-        evaluateGuess(letter);
-        }
-    }
-    updateDisplay();
-    checkWin();
-};
-
-function evaluateGuess(letter){
-    var positions = [] //array for storing letter positions
-    
-    for(var i = 0; i < anime[currentWord].length; i++){
-        if(anime[currentWord[i]] === letter){
-            positions.push(i)
-        }
-    }
-    
-    if(positions.length <= 0){
-    remainingGuesses--;
-    } else {
-        for(var i = 0; i < positions.length; i++){
-            anime[positions[i]] = letter;
-        }
-    }
-};
-
-function checkWin{
-    if(anime.indexOf("_") === -1){
-        document.getElementById("pressKeyTryAgain").style.cssText="display: block";
-        wins++;
-        gameFinished = true;
-    }
+//update yo man
+function updateHangmanImage() {
+	document.getElementById("hamgmanImage").src = "images/" + (maxtries - remainingGuesses) + ".png"
 }
