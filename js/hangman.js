383c9edc2a1132281004c954c2bcbb4d413f32a7
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-02-05 18:18:42 -0500

    hang 10

--- js/hangman.js
+++ js/hangman.js
@@ -1,4 +1,4 @@
-var selectableWords=["naruto", "bleach", "onepiece", "dragonball", "inuyasha", "hunterxhunter", "pokemon", "digimon", "monster", "onepunchman", "fairytail", "akira"];
+var selectableWords = ["naruto", "bleach", "onepiece", "dragonball", "inuyasha", "hunterxhunter", "pokemon", "digimon", "monster", "onepunchman", "fairytail", "akira"];
 
 
 const maxTries = 10; // maximum number of attempts
@@ -31,7 +31,7 @@ function resetGame() {
     }
 	
 	//hide game over and win images
-	document.getElementById("pressKeytryAgain").style.cssText = "display: none";
+	document.getElementById("pressKeyTryAgain").style.cssText = "display: none";
 	document.getElementById("gameover-image").style.cssText = "display: none";
 	document.getElementById("youwin-image").style.cssText = "display: none";
 	
@@ -66,3 +66,82 @@ function updateDisplay() {
 function updateHangmanImage() {
 	document.getElementById("hamgmanImage").src = "images/" + (maxtries - remainingGuesses) + ".png"
 }
+
+document.onkeydown = function(event){
+	//reset if we finish a game
+	if(hasFinished){
+		resetGame();
+		hasFinished = false;
+	}else{
+		//check to make sure a-z clicked
+		if(event.keyCode >= 65 && event.keycode <=90){
+			makeGuess(event.key.toLowerCase());
+		}
+	}
+	
+};
+
+///Guessing Function
+
+function makeGuess(letter) {
+	if(remainingGuesses > 0){
+		if(!gameStarted){
+			gameStarted = true;
+		}
+		if(guessedLetters.indexOf(letter) === -1){
+			guessedLetters.push(letter);
+			evaluateGuess(letter);
+		}
+		
+	}
+	
+	updateDisplay();
+	checkWin();
+};
+
+/////Check guesses
+function evaluateGuess(letter){
+	var positions = [];
+	
+	for(var i = 0; i < SelectableWords[currentWordIndex].length; i++){
+		if(selectableWords[currentWordIndex][i] === letter){
+		positions.push(i);
+		}
+			
+	}
+	if (positions.length <= 0){
+		remainingGuesses--
+		updateHangmanImage();
+	}else{
+		for(var i = 0; i < positions.length; i++){
+			guessingWord[positions[i]] = letter;
+		}
+	}
+
+};
+
+
+function Checkwin(){
+	if(guessingWord.indexOf("_") === -1){
+		document.getElementById("youwin-image").style.cssText = "display: block";
+		document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
+		winss++;
+		hasFinished = true;
+		
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
